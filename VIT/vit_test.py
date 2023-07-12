09d8be7ab4f9482836d95ae86e0cb0287bb00a45
chenzhuo <a283910020@gmail.com>
2023-07-12 23:02:34 +0800

    VIT 2nd version

--- VIT/vit_test.py
+++ VIT/vit_test.py
@@ -8,31 +8,14 @@ import cv2
 from PIL import Image
 
 
-
 batch_size_train = 128
 batch_size_test = 1
 img_size = 256
 lr = 0.0001
 
-def test():
-    v = ViT(
-        image_size=256,
-        patch_size=32,
-        num_classes=2,
-        dim=1024,
-        depth=6,
-        heads=16,
-        mlp_dim=2048,
-        dropout=0.1,
-        emb_dropout=0.1
-    )
 
-    img = torch.randn(10, 3, 256, 256)
 
-    preds = v(img)
-    print(preds)
-    # assert preds.shape == (100, 20), 'correct logits outputted'
-    # print("test pass")
+
 
 def box_crop(img, crop_size=224):
     h, w, c = img.shape
@@ -44,7 +27,6 @@ def box_crop(img, crop_size=224):
     y2 = y + crop_size // 2
     # return x1, y1, x2, y2
     return img[y1:y2, x1:x2, :]
-
 
 def img_crop():
     src_folder = '/Users/chenzhuo/Desktop/github/UGATIT/dataset/selfie2anime'
@@ -62,32 +44,38 @@ def img_crop():
             cv2.imwrite(os.path.join(dest, img), img_crop)
 
 
-# train_transform = transforms.Compose([
-#             transforms.RandomHorizontalFlip(),
-#             # transforms.Resize((img_size + 30, img_size+30)),
-#             # transforms.RandomCrop(img_size),
-#             transforms.ToTensor(),
-#             transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
-#         ])
-# test_transform = transforms.Compose([
-#             # transforms.RandomHorizontalFlip(),
-#             # transforms.Resize((img_size, img_size)),
-#             transforms.ToTensor(),
-#             transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
-#         ])
-trans = transforms.Compose([
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
-        ])
-# define the dataset & transfer to dataloader
-trainA = ImageFolder(os.path.join('data', 'train'), trans)
-# trainB = ImageFolder(os.path.join('data', 'trainB'), trans)
-testA = ImageFolder(os.path.join('data', 'test'), trans)
-# testB = ImageFolder(os.path.join('data', 'testB'), trans)
-trainA_loader = DataLoader(trainA, batch_size=batch_size_train, shuffle=True)
-trainB_loader = DataLoader(testA, batch_size=batch_size_train, shuffle=True)
+def build_dataset():
+    trans = transforms.Compose([
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
+            ])
+    # define the dataset & transfer to dataloader
+    train = ImageFolder(os.path.join('data', 'train'), trans)
+    test = ImageFolder(os.path.join('data', 'test'), trans)
+    train_loader = DataLoader(train, batch_size=batch_size_train, shuffle=True)
+    test_loader = DataLoader(test, batch_size=batch_size_train, shuffle=True)
+    return train_loader, test_loader
+
+
+def train():
+    pass
+
+
 if __name__ == '__main__':
-    test()
-    print(trainA_loader)
+    model = ViT(
+        image_size=256,
+        patch_size=32,
+        num_classes=2,
+        dim=1024,
+        depth=6,
+        heads=16,
+        mlp_dim=2048,
+        dropout=0.1,
+        emb_dropout=0.1
+    )
+
+    train_loader, test_loader = build_dataset()
+
+    print(train_loader)
     # img_crop()
